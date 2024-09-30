a916799dc0df00af5519e774078bab52e204b986
Baraaabudraz <abudrazbaraa@gmail.com>
2024-09-30 13:02:11 +0300

    rearrange route

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,28 @@ use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
+
+//Route::get('/', function () {
+//    return view('cms.dashboard');
+//})->name('dashboard');
+
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/', function () {
-        return view('cms.dashboard');
-    })->name('dashboard');
+    Route::prefix('cms')->group(function (){
+        Route::view('/','cms.dashboard')->name('dashboard');
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
 });
