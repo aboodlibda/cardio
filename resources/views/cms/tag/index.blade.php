d4b16acefdfe56deb434ac397921de3cbc1d4206
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-02 13:40:20 +0300

    Update index.blade.php

--- resources/views/cms/tag/index.blade.php
+++ resources/views/cms/tag/index.blade.php
@@ -1,6 +1,11 @@
 @extends('cms.layout.master')
 @section('title',trans('dashboard_trans.Tags'))
 @section('style')
+    @if(App::getLocale()=='ar')
+        <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.rtl.css')}}" rel="stylesheet" type="text/css">
+    @else
+        <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
+    @endif
 @endsection
 @section('content')
     <!--begin::Page title-->
@@ -112,41 +117,115 @@
     <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
         <!--begin::Container-->
         <div class="container-xxl" id="kt_content_container">
-            <!--begin::Card-->
-            <div class="card">
-                <!--begin::Card body-->
-                <div class="card-body">
-                    <!--begin::Heading-->
-                    <div class="card-px text-center pt-15 pb-15">
-                        <!--begin::Title-->
-                        <h2 class="fs-2x fw-bold mb-0">{{trans('dashboard_trans.Add New Tag')}}</h2>
-                        <!--end::Title-->
-                        <!--begin::Description-->
-                        <p class="text-gray-400 fs-4 fw-semibold py-7">{{trans('dashboard_trans.Click on the below buttons to launch')}}
-                            <br />a new tag.</p>
-                        <!--end::Description-->
-                        <!--begin::Action-->
+            <!--begin::Category-->
+            <div class="card card-flush">
+                <!--begin::Card header-->
+                <div class="card-header align-items-center py-5 gap-2 gap-md-5">
+                    <!--begin::Card title-->
+                    <div class="card-title">
+                        <!--begin::Search-->
+                        <div class="d-flex align-items-center position-relative my-1">
+                            <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>
+                            <input type="text" data-kt-ecommerce-category-filter="search" class="form-control form-control-solid w-250px ps-12" placeholder="Search Category" />
+                        </div>
+                        <!--end::Search-->
+                    </div>
+                    <!--end::Card title-->
+                    <!--begin::Card toolbar-->
+                    <div class="card-toolbar">
+                        <!--begin::Add customer-->
                         <a href="#" class="btn btn-primary er fs-6 px-8 py-4" data-bs-toggle="modal" data-bs-target="#kt_modal_new_target">{{trans('dashboard_trans.Add New Tag')}}</a>
-                        <!--end::Action-->
+                        <!--end::Add customer-->
                     </div>
-                    <!--end::Heading-->
-                    <!--begin::Illustration-->
-                    <div class="text-center pb-15 px-5">
-                        <img src="assets/media/illustrations/dozzy-1/17.png" alt="" class="mw-100 h-200px h-sm-325px" />
-                    </div>
-                    <!--end::Illustration-->
+                    <!--end::Card toolbar-->
+                </div>
+                <!--end::Card header-->
+                <!--begin::Card body-->
+                <div class="card-body pt-0">
+                    <!--begin::Table-->
+                    <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_category_table">
+                        <thead>
+                        <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                            <th class="w-10px pe-2">
+                                <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+                                    <input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_ecommerce_category_table .form-check-input" value="1" />
+                                </div>
+                            </th>
+                            <th class="min-w-250px">{{trans('dashboard_trans.Tags')}}</th>
+                            <th class="min-w-150px">{{trans('dashboard_trans.Icon')}}</th>
+                            <th class="text-end min-w-70px">{{trans('dashboard_trans.Actions')}}</th>
+                        </tr>
+                        </thead>
+                        <tbody class="fw-semibold text-gray-600">
+                        <tr>
+                            <td>
+                                <div class="form-check form-check-sm form-check-custom form-check-solid">
+                                    <input class="form-check-input" type="checkbox" value="1" />
+                                </div>
+                            </td>
+                            <td>
+                                <div class="d-flex">
+                                    <!--begin::Thumbnail-->
+                                    <a href="../../demo3/dist/apps/ecommerce/catalog/edit-category.html" class="symbol symbol-50px">
+                                        <span class="symbol-label" style="background-image:url({{url('assets/media//stock/ecommerce/68.png')}});"></span>
+                                    </a>
+                                    <!--end::Thumbnail-->
+                                    <div class="ms-5">
+                                        <!--begin::Title-->
+                                        <a href="../../demo3/dist/apps/ecommerce/catalog/edit-category.html" class="text-gray-800 text-hover-primary fs-5 fw-bold mb-1" data-kt-ecommerce-category-filter="category_name">Computers</a>
+                                        <!--end::Title-->
+                                        <!--begin::Description-->
+                                        <div class="text-muted fs-7 fw-bold">Our computers and tablets include all the big brands.</div>
+                                        <!--end::Description-->
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <!--begin::Badges-->
+                                <div class="badge badge-light-success">Automated</div>
+                                <!--end::Badges-->
+                            </td>
+                            <td class="text-end">
+                                <a href="#" class="btn btn-sm btn-light btn-active-light-primary btn-flex btn-center" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
+                                    <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
+                                <!--begin::Menu-->
+                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
+                                    <!--begin::Menu item-->
+                                    <div class="menu-item px-3">
+                                        <a href="../../demo3/dist/apps/ecommerce/catalog/add-category.html" class="menu-link px-3">{{trans('dashboard_trans.Edit')}}</a>
+                                    </div>
+                                    <!--end::Menu item-->
+                                    <!--begin::Menu item-->
+                                    <div class="menu-item px-3">
+                                        <a href="#" class="menu-link px-3" data-kt-ecommerce-category-filter="delete_row">{{trans('dashboard_trans.Delete')}}</a>
+                                    </div>
+                                    <!--end::Menu item-->
+                                </div>
+                                <!--end::Menu-->
+                            </td>
+                        </tr>
+                        </tbody>
+                        <!--end::Table body-->
+                    </table>
+                    <!--end::Table-->
                 </div>
                 <!--end::Card body-->
             </div>
-            <!--end::Card-->
+            <!--end::Category-->
         </div>
         <!--end::Container-->
     </div>
     <!--end::Content-->
 @endsection
 @section('script')
-    <script src="{{asset('assets/js/widgets.bundle.js')}}"></script>
-    <script src="{{asset('assets/js/custom/widgets.js')}}"></script>
+    <script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
+    <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/categories.js')}}"></script>
+
+{{--    <script src="{{asset('assets/js/widgets.bundle.js')}}"></script>--}}
+{{--    <script src="{{asset('assets/js/custom/widgets.js')}}"></script>--}}
     <script src="{{asset('assets/js/custom/utilities/modals/new-target.js')}}"></script>
     <script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
 @endsection
