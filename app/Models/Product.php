9ca08670cbea31886cef266d48660973ade8f86a
aboodlibda <aboodlibda@gmail.com>
2024-10-05 13:43:55 +0300

    create ptoducts

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    protected $guarded = [];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
+}
---
+++ database/migrations/2024_10_05_125402_create_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('price');
+            $table->enum('status',['published','unpublished','draft'])->default('draft');
+            $table->foreignId('user_id');
+            $table->text('slug')->unique();
+            $table->string('quantity');
+            $table->string('SKU')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
