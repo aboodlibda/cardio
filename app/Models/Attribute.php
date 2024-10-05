3f2797e28ba60f494a3232798dabe339b0bffbef
aboodlibda <aboodlibda@gmail.com>
2024-10-05 14:07:41 +0300

    Update Attribute.php

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Attribute extends Model
 {
     protected $guarded = [];
 
-    public function products()
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
     }
