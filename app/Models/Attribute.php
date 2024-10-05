bbbd8cb6ea759fc7b4db45a9b1f54e04d7a65abf
aboodlibda <aboodlibda@gmail.com>
2024-10-05 14:26:32 +0300

    create attribute values

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attribute extends Model
 {
@@ -12,5 +13,10 @@ class Attribute extends Model
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function attribute_values(): HasMany
+    {
+        return $this->hasMany(AttributeValues::class);
     }
 }
