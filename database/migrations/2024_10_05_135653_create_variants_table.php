522ba96b1bd85b2d510baed63b8fb0f390888d2e
aboodlibda <aboodlibda@gmail.com>
2024-10-05 15:02:30 +0300

    Update 2024_10_05_135653_create_variants_table.php

--- database/migrations/2024_10_05_135653_create_variants_table.php
+++ database/migrations/2024_10_05_135653_create_variants_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('variants', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
