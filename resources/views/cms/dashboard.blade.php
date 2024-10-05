8d670005405310927dacc498ca35f3db2ffe53d0
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-05 10:31:30 +0300

    Update dashboard.blade.php

--- resources/views/cms/dashboard.blade.php
+++ resources/views/cms/dashboard.blade.php
@@ -4,7 +4,7 @@
     <!--begin::Page title-->
     <div class="page-title d-flex flex-column align-items-start justify-content-center flex-wrap me-lg-2 pb-10 pb-lg-0" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', lg: '#kt_header_container'}">
         <!--begin::Heading-->
-        <h1 class="d-flex flex-column text-dark fw-bold my-0 fs-1">{{trans('dashboard_trans.Hello')}}, Paul
+        <h1 class="d-flex flex-column text-dark fw-bold my-0 fs-1">{{trans('dashboard_trans.Hello')}}, Admin
             <small class="text-muted fs-6 fw-semibold pt-1">{{trans('dashboard_trans.You have got new sales')}}</small></h1>
         <!--end::Heading-->
     </div>
@@ -39,7 +39,7 @@
                                 <span class="text-dark fw-bold fs-3x me-2 lh-0">560</span>
                                 <!--end::Number-->
                                 <!--begin::Text-->
-                                <span class="text-dark fw-bold fs-6 lh-0">+ 28% this week</span>
+                                <span class="text-dark fw-bold fs-6 lh-0">+ 28% {{trans('dashboard_trans.this week')}}</span>
                                 <!--end::Text-->
                             </div>
                             <!--end::Stats-->
@@ -71,7 +71,7 @@
                                 <span class="text-dark fw-bold fs-3x me-2 lh-0">47</span>
                                 <!--end::Number-->
                                 <!--begin::Text-->
-                                <span class="text-dark fw-bold fs-6 lh-0">- 12% this week</span>
+                                <span class="text-dark fw-bold fs-6 lh-0">- 12% {{trans('dashboard_trans.this week')}}</span>
                                 <!--end::Text-->
                             </div>
                             <!--end::Stats-->
@@ -111,7 +111,7 @@
                                         <!--begin::Title-->
                                         <div>
                                             <div class="fs-5 text-dark fw-bold lh-1">$50K</div>
-                                            <div class="fs-7 text-gray-600 fw-bold">Sales</div>
+                                            <div class="fs-7 text-gray-600 fw-bold">{{trans('dashboard_trans.Sales')}}</div>
                                         </div>
                                         <!--end::Title-->
                                     </div>
@@ -133,7 +133,7 @@
                                         <!--begin::Title-->
                                         <div>
                                             <div class="fs-5 text-dark fw-bold lh-1">$4,5K</div>
-                                            <div class="fs-7 text-gray-600 fw-bold">Revenue</div>
+                                            <div class="fs-7 text-gray-600 fw-bold">{{trans('dashboard_trans.Revenue')}}</div>
                                         </div>
                                         <!--end::Title-->
                                     </div>
@@ -155,7 +155,7 @@
                                         <!--begin::Title-->
                                         <div>
                                             <div class="fs-5 text-dark fw-bold lh-1">40</div>
-                                            <div class="fs-7 text-gray-600 fw-bold">Tasks</div>
+                                            <div class="fs-7 text-gray-600 fw-bold">{{trans('dashboard_trans.Tasks')}}</div>
                                         </div>
                                         <!--end::Title-->
                                     </div>
@@ -205,7 +205,7 @@
                         <div class="card-header align-items-center border-0 mt-4">
                             <h3 class="card-title align-items-start flex-column">
                                 <span class="fw-bold mb-2 text-dark">{{trans('dashboard_trans.Activities')}}</span>
-                                <span class="text-muted fw-semibold fs-7">890,344 Sales</span>
+                                <span class="text-muted fw-semibold fs-7">890,344 {{trans('dashboard_trans.Sales')}}</span>
                             </h3>
                             <div class="card-toolbar">
                                 <!--begin::Menu-->
@@ -432,220 +432,6 @@
                         <!--end: Card Body-->
                     </div>
                     <!--end: List Widget 5-->
-                </div>
-                <!--end::Col-->
-                <!--begin::Col-->
-                <div class="col-xxl-6">
-                    <!--begin::List Widget 4-->
-                    <div class="card card-xl-stretch mb-5 mb-xl-10">
-                        <!--begin::Header-->
-                        <div class="card-header border-0 pt-5">
-                            <h3 class="card-title align-items-start flex-column">
-                                <span class="card-label fw-bold text-dark">Trends</span>
-                                <span class="text-muted mt-1 fw-semibold fs-7">Latest tech trends</span>
-                            </h3>
-                            <div class="card-toolbar">
-                                <!--begin::Menu-->
-                                <button type="button" class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                    <i class="ki-duotone ki-category fs-6">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                        <span class="path3"></span>
-                                        <span class="path4"></span>
-                                    </i>
-                                </button>
-                                <!--begin::Menu 3-->
-                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-3" data-kt-menu="true">
-                                    <!--begin::Heading-->
-                                    <div class="menu-item px-3">
-                                        <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">Payments</div>
-                                    </div>
-                                    <!--end::Heading-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <a href="#" class="menu-link px-3">Create Invoice</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <a href="#" class="menu-link flex-stack px-3">Create Payment
-                                            <span class="ms-2" data-bs-toggle="tooltip" title="Specify a target name for future usage and reference">
-															<i class="ki-duotone ki-information fs-6">
-																<span class="path1"></span>
-																<span class="path2"></span>
-																<span class="path3"></span>
-															</i>
-														</span></a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3">
-                                        <a href="#" class="menu-link px-3">Generate Bill</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3" data-kt-menu-trigger="hover" data-kt-menu-placement="right-end">
-                                        <a href="#" class="menu-link px-3">
-                                            <span class="menu-title">Subscription</span>
-                                            <span class="menu-arrow"></span>
-                                        </a>
-                                        <!--begin::Menu sub-->
-                                        <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3">Plans</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3">Billing</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <a href="#" class="menu-link px-3">Statements</a>
-                                            </div>
-                                            <!--end::Menu item-->
-                                            <!--begin::Menu separator-->
-                                            <div class="separator my-2"></div>
-                                            <!--end::Menu separator-->
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <div class="menu-content px-3">
-                                                    <!--begin::Switch-->
-                                                    <label class="form-check form-switch form-check-custom form-check-solid">
-                                                        <!--begin::Input-->
-                                                        <input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-                                                        <!--end::Input-->
-                                                        <!--end::Label-->
-                                                        <span class="form-check-label text-muted fs-6">Recuring</span>
-                                                        <!--end::Label-->
-                                                    </label>
-                                                    <!--end::Switch-->
-                                                </div>
-                                            </div>
-                                            <!--end::Menu item-->
-                                        </div>
-                                        <!--end::Menu sub-->
-                                    </div>
-                                    <!--end::Menu item-->
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3 my-1">
-                                        <a href="#" class="menu-link px-3">Settings</a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                </div>
-                                <!--end::Menu 3-->
-                                <!--end::Menu-->
-                            </div>
-                        </div>
-                        <!--end::Header-->
-                        <!--begin::Body-->
-                        <div class="card-body pt-5">
-                            <!--begin::Item-->
-                            <div class="d-flex align-items-sm-center mb-7">
-                                <!--begin::Symbol-->
-                                <div class="symbol symbol-50px me-5">
-													<span class="symbol-label">
-														<img src="assets/media/svg/brand-logos/plurk.svg" class="h-50 align-self-center" alt="" />
-													</span>
-                                </div>
-                                <!--end::Symbol-->
-                                <!--begin::Section-->
-                                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
-                                    <div class="flex-grow-1 me-2">
-                                        <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold">Top Authors</a>
-                                        <span class="text-muted fw-semibold d-block fs-7">Mark, Rowling, Esther</span>
-                                    </div>
-                                    <span class="badge badge-light fw-bold my-2">+82$</span>
-                                </div>
-                                <!--end::Section-->
-                            </div>
-                            <!--end::Item-->
-                            <!--begin::Item-->
-                            <div class="d-flex align-items-sm-center mb-7">
-                                <!--begin::Symbol-->
-                                <div class="symbol symbol-50px me-5">
-													<span class="symbol-label">
-														<img src="assets/media/svg/brand-logos/telegram.svg" class="h-50 align-self-center" alt="" />
-													</span>
-                                </div>
-                                <!--end::Symbol-->
-                                <!--begin::Section-->
-                                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
-                                    <div class="flex-grow-1 me-2">
-                                        <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold">Popular Authors</a>
-                                        <span class="text-muted fw-semibold d-block fs-7">Randy, Steve, Mike</span>
-                                    </div>
-                                    <span class="badge badge-light fw-bold my-2">+280$</span>
-                                </div>
-                                <!--end::Section-->
-                            </div>
-                            <!--end::Item-->
-                            <!--begin::Item-->
-                            <div class="d-flex align-items-sm-center mb-7">
-                                <!--begin::Symbol-->
-                                <div class="symbol symbol-50px me-5">
-													<span class="symbol-label">
-														<img src="assets/media/svg/brand-logos/vimeo.svg" class="h-50 align-self-center" alt="" />
-													</span>
-                                </div>
-                                <!--end::Symbol-->
-                                <!--begin::Section-->
-                                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
-                                    <div class="flex-grow-1 me-2">
-                                        <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold">New Users</a>
-                                        <span class="text-muted fw-semibold d-block fs-7">John, Pat, Jimmy</span>
-                                    </div>
-                                    <span class="badge badge-light fw-bold my-2">+4500$</span>
-                                </div>
-                                <!--end::Section-->
-                            </div>
-                            <!--end::Item-->
-                            <!--begin::Item-->
-                            <div class="d-flex align-items-sm-center mb-7">
-                                <!--begin::Symbol-->
-                                <div class="symbol symbol-50px me-5">
-													<span class="symbol-label">
-														<img src="assets/media/svg/brand-logos/bebo.svg" class="h-50 align-self-center" alt="" />
-													</span>
-                                </div>
-                                <!--end::Symbol-->
-                                <!--begin::Section-->
-                                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
-                                    <div class="flex-grow-1 me-2">
-                                        <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold">Active Customers</a>
-                                        <span class="text-muted fw-semibold d-block fs-7">Mark, Rowling, Esther</span>
-                                    </div>
-                                    <span class="badge badge-light fw-bold my-2">+686$</span>
-                                </div>
-                                <!--end::Section-->
-                            </div>
-                            <!--end::Item-->
-                            <!--begin::Item-->
-                            <div class="d-flex align-items-sm-center mb-7">
-                                <!--begin::Symbol-->
-                                <div class="symbol symbol-50px me-5">
-													<span class="symbol-label">
-														<img src="assets/media/svg/brand-logos/kickstarter.svg" class="h-50 align-self-center" alt="" />
-													</span>
-                                </div>
-                                <!--end::Symbol-->
-                                <!--begin::Section-->
-                                <div class="d-flex align-items-center flex-row-fluid flex-wrap">
-                                    <div class="flex-grow-1 me-2">
-                                        <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold">Bestseller Theme</a>
-                                        <span class="text-muted fw-semibold d-block fs-7">Disco, Retro, Sports</span>
-                                    </div>
-                                    <span class="badge badge-light fw-bold my-2">+726$</span>
-                                </div>
-                                <!--end::Section-->
-                            </div>
-                            <!--end::Item-->
-                        </div>
-                        <!--end::Body-->
-                    </div>
-                    <!--end::List Widget 4-->
                 </div>
                 <!--end::Col-->
             </div>
