021df4da3d02b79274222ecda60dd21382ebf2e7
aboodlibda <aboodlibda@gmail.com>
2024-10-05 15:02:22 +0300

    Update 2024_10_05_142017_create_attribute_values_table.php

--- database/migrations/2024_10_05_142017_create_attribute_values_table.php
+++ database/migrations/2024_10_05_142017_create_attribute_values_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('attribute_values', function (Blueprint $table) {
             $table->id();
             $table->string('value');
-            $table->foreignId('attribute_id');
+            $table->foreignId('attribute_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
