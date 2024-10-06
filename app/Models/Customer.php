434bb336ed97d462a2a1c83fd1fce52e24ac4a47
aboodlibda <aboodlibda@gmail.com>
2024-10-06 10:15:45 +0300

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
     protected $guarded = [];
+
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class);
+    }
 }
