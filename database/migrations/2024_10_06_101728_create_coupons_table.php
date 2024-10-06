d780f81e23d473e6652d067279a15db108cb8cda
aboodlibda <aboodlibda@gmail.com>
2024-10-06 10:34:31 +0300

    create coupons

---
+++ database/migrations/2024_10_06_101728_create_coupons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->text('description')->nullable();
+            $table->string('discount_value');
+            $table->enum('discount_type',['percentage','fixed']);
+            $table->string('times_used')->default(0);
+            $table->string('max_used');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
