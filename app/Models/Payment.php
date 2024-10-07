4d24f9e76cf2a35654aabbf78aa2324ef76c5fb0
aboodlibda <aboodlibda@gmail.com>
2024-10-07 10:35:26 +0300

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Payment extends Model
 {
     protected $guarded = [];
 
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
