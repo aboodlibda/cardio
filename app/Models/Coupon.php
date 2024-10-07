e02e78cd9dec57d6b6cc0ed708c501f6ac873e0d
aboodlibda <aboodlibda@gmail.com>
2024-10-07 10:53:52 +0300

    Update Coupon.php

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Coupon extends Model
 {
@@ -12,5 +13,10 @@ class Coupon extends Model
     public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
