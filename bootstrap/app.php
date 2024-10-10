08130c1dda90d8d290d2819dffb785f5844fac07
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-10 10:58:11 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LimitRequest;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,23 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+
+            'limit_request'=>LimitRequest::class,
+            'authenticated'=>\App\Http\Middleware\Authenticated::class,
+
+        ]);
+//        $middleware->redirectGuestsTo(function ($request) {
+//            if (!$request->expectsJson()) {
+//                if (in_array('auth:user', $request->route()->middleware())) {
+//                    if (!auth('user')->check()) {
+//                        return route('sign-in');
+//                    }
+//                }
+//            }
+//        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+
     })->create();
