6e6338e9cf068580bc4e60b681fded6b9c539eab
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-10 10:58:07 +0300

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Role extends Model
 {
-    use HasFactory;
+    public $guarded = [];
+
+    public function users():HasMany
+    {
+        return $this->hasMany(User::class);
+
+    }
+
+    public function permissions():hasMany
+    {
+        return $this->hasMany(Permission::class);
+
+    }
 }
