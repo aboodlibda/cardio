a952c3939c6f10525b654f55972a4315be3dfaa9
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-10 10:57:59 +0300

    Update RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     public function index()
     {
-        return view('cms.user.role.index');
+        $roles = Role::query()->with(['users','permissions'])->get();
+        return view('cms.user.role.index',compact('roles'));
     }
 
     public function create()
@@ -19,10 +21,25 @@ class RoleController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'name'=>'required|string',
+            'description'=>'nullable|string',
+          ]);
+        $data = $request->only(['name']);
+        $is_saved = Role::query()->create($data);
+        if ($is_saved){
+            notify()->success(trans('dashboard_trans.Role created successfully'));
+            return redirect()->back();
+        }else{
+            notify()->error(trans('dashboard_trans.Failed to create role!'));
+            return redirect()->back();
+        }
     }
 
     public function show($id)
     {
+        $role = Role::query()->with(['users','permissions'])->findOrFail($id);
+        return view('cms.user.role.show',compact('role'));
     }
 
     public function edit($id)
