b860a177bb51f1c02297b2f5eaa1eae5d5829d7e
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-11 12:31:40 +0300

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+    }
     public function showLogin()
     {
         return view('cms.auth.sign-in');
@@ -16,7 +19,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email',
+            'email' => 'required|email|exists:users,email',
             'password' => 'required',
         ]);
 
@@ -25,12 +28,16 @@ class LoginController extends Controller
         if (Auth::guard('user')->attempt($credentials)) {
             $user = Auth::guard('user')->user();
             if ($user->status == 'active') {
+                notify()->success(trans('dashboard_trans.Sign in successfully'));
                 return redirect()->route('dashboard');
             } else {
-                notify()->error('Error Credentials');
+                notify()->error(trans('dashboard_trans.This User InActive'));
                 return redirect()->back()->withInput();
             }
 
+        }else{
+            notify()->error(trans('dashboard_trans.Error Credentials'));
+            return redirect()->back()->withInput();
         }
 
     }
