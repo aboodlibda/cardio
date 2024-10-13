f70a6b2bc61c8846f620905f1638b3f43de0ee3d
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-13 11:34:28 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
@@ -45,11 +46,7 @@ class UserController extends Controller
           ]);
 
         $data = $this->getData($request);
-
-
-
-
-
+        $data['password'] = Hash::make($request->password);
         $is_Saved = User::query()->create($data);
 
         if ($is_Saved){
@@ -74,10 +71,11 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $request->request->add(['id'=>$id]);
-        $request->validate([
+
+        $validator = Validator::make($request->all(), [
             'name'  => 'required|string|min:3|max:200,name,'.$id,
             'email' => 'required|email|unique:users,email,'.$id,
-            'password'         => ['required','current_password:user',Password::min(8)
+            'password'         => ['current_password:user',Password::min(8)
                 ->letters()
                 ->mixedCase()
                 ->numbers()
@@ -87,27 +85,53 @@ class UserController extends Controller
             'user_name' => 'required|string|min:3|max:15|not_regex:[!@#$%^&*()]|unique:users,user_name,'.$id,
             'phone_number'  => 'required|numeric',
             'gender'  => 'required|in:male,female',
-            'role_id'  => 'required|int|exists:roles,id',
+            'role_id'  => 'int|exists:roles,id',
             'status'  => 'in:on',
             'avatar'  => 'nullable|image',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
 
         $data = $this->getData($request);
 
         $is_Updated = User::query()->find($id)->update($data);
 
         if ($is_Updated){
-            notify()->success(trans('dashboard_trans.User Updated successfully'));
+//            notify()->success(trans('dashboard_trans.User Updated successfully'));
+            return response()->json([
+                'icon' => 'success',
+                'confirmButtonText'=>'Ok, got it!',
+                'text' => trans('dashboard_trans.User Updated successfully'),
+            ]);
         }else{
-            notify()->error(trans('dashboard_trans.Failed to update this user!'));
+//            notify()->error(trans('dashboard_trans.Failed to update this user!'));
+            return response()->json(['error' => 'Something went wrong'], 500);
 
         }
-        return redirect()->back();
+//        return redirect()->back();
 
     }
 
     public function destroy($id)
     {
+        $is_Deleted = User::destroy($id);
+        if ($is_Deleted){
+            return response()->json([
+                'title'=>'success',
+                'icon'=>'success',
+                'text'=>trans('dashboard_trans.User deleted successfully'),
+            ]);
+
+        }else{
+            return response()->json([
+                'title'=>'error',
+                'icon'=>'error',
+                'text'=>trans('dashboard_trans.Failed to delete this user!'),
+
+            ]);
+        }
     }
 
 
@@ -117,7 +141,7 @@ class UserController extends Controller
             'name', 'email', 'phone_number', 'user_name', 'gender', 'role_id', 'avatar'
         ]);
 
-        $data['password'] = Hash::make($request->password);
+
         $data['status'] = $request->has('status') ? 'active' : 'inactive';
 
         if ($request->hasFile('avatar')) {
@@ -127,5 +151,89 @@ class UserController extends Controller
             $data['avatar'] = $imageName;
         }
         return $data;
+    }
+
+    public function updateEmail(Request $request,$id){
+
+        $request->request->add(['id'=>$id]);
+
+        $is_Updated =  User::find($id);
+
+        $is_Updated->Update([
+            'email'=>$request->email,
+        ]);
+        if ($is_Updated){
+            return response()->json([
+                'icon' => 'success',
+                'confirmButtonText'=>'Ok, got it!',
+                'text' => trans('dashboard_trans.Email Updated Successfully'),
+            ]);
+        }else{
+            notify()->error(trans('dashboard_trans.Failed to update email'));
+        }
+        return redirect()->back();
+    }
+    public function updatePassword(Request $request,$id){
+
+        $request->request->add(['id'=>$id]);
+        $request->validate([
+            'password'         => ['required',Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()
+            ],
+            'current_password'         => ['required','current_password:user',Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()
+            ],
+            'confirm_password'         => ['required','confirmed:password',Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()
+            ],
+
+        ]);
+
+        $is_Updated =  User::find($id);
+
+        $is_Updated->Update([
+            'password'=>$request->password,
+        ]);
+        if ($is_Updated){
+            return response()->json([
+                'icon' => 'success',
+                'confirmButtonText'=>'Ok, got it!',
+                'text' => trans('dashboard_trans.Password Updated Successfully'),
+            ]);
+        }else{
+            return response()->json([
+                'icon' => 'error',
+                'confirmButtonText'=>'Ok, got it!',
+                'text' => trans('dashboard_trans.Failed to update password'),
+            ]);
+        }
+    }
+    public function updateRole(Request $request,$id){
+
+        $request->request->add(['id'=>$id]);
+
+        $is_Updated =  User::find($id);
+
+        $is_Updated->Update([
+            'role_id' => $request->role_id,
+        ]);
+        if ($is_Updated){
+            notify()->success(trans('dashboard_trans.Email Updated Successfully'));
+        }else{
+            notify()->error(trans('dashboard_trans.Failed to update email'));
+        }
+        return redirect()->back();
     }
 }
