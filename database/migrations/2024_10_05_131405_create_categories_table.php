f756dd80bd599f294285de75662f1c7d6056531a
Baraaabudraz <abudrazbaraa@gmail.com>
2024-10-16 15:34:19 +0300

    Update 2024_10_05_131405_create_categories_table.php

--- database/migrations/2024_10_05_131405_create_categories_table.php
+++ database/migrations/2024_10_05_131405_create_categories_table.php
@@ -10,8 +10,8 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->integer('parent_id')->nullable();
-            $table->string('name');
-            $table->text('description')->nullable();
+            $table->json('name');
+            $table->json('description')->nullable();
             $table->string('image')->nullable();
             $table->enum('status',['active','inactive']);
             $table->string('slug')->unique()->nullable();
