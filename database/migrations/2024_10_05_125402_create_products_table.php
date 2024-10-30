b791a12821ed295dbe9775b738b5427ab6b71e62
Baraaabudraz <102161963+Baraaabudraz@users.noreply.github.com>
2024-10-30 10:36:32 +0200

    Update 2024_10_05_125402_create_products_table.php

--- database/migrations/2024_10_05_125402_create_products_table.php
+++ database/migrations/2024_10_05_125402_create_products_table.php
@@ -9,13 +9,18 @@ return new class extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->json('title');
+            $table->json('name');
             $table->json('description');
             $table->string('price');
             $table->enum('status',['published','unpublished','draft'])->default('draft');
             $table->foreignId('user_id');
             $table->string('slug')->unique();
             $table->string('quantity');
+            $table->string('image');
+            $table->string('discounted_price');
+            $table->string('vat_amount')->nullable();
+            $table->enum('discount_type',['no_discount','percentage','fixed_price']);
+            $table->enum('tax_type',['free','taxable_goods','downloadable_product']);
             $table->string('SKU')->unique()->nullable();
             $table->timestamps();
         });
