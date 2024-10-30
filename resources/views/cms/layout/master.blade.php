14e69c1a73e7bbd507ef5cd4104b2cd93bc48c84
Baraaabudraz <102161963+Baraaabudraz@users.noreply.github.com>
2024-10-30 11:09:18 +0200

    Reapply "Update master.blade.php"
    This reverts commit 9d3aad3305060d35a55f158554eb23de7d7aea66.

--- resources/views/cms/layout/master.blade.php
+++ resources/views/cms/layout/master.blade.php
@@ -19,6 +19,7 @@
     <meta property="og:title" content="Cardio"/>
     <meta property="og:url" content="https://uplancerps.com"/>
     <meta property="og:site_name" content="Cardio"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="canonical" href="Https://https://uplancerps.com"/>
     <link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
     <!--begin::Fonts-->
@@ -575,7 +576,7 @@
                     <div class="d-flex align-items-center">
                         <!--begin::Avatar-->
                         <div class="symbol symbol-circle symbol-40px">
-                            <img src="{{asset('images/users/'.Auth::user()->avatar)}}" alt="photo" />
+                            <img src="{{asset('storage/'.Auth::user()->avatar)}}" alt="photo" />
                         </div>
                         <!--end::Avatar-->
                         <!--begin::User info-->
@@ -605,7 +606,7 @@
                                 <div class="menu-content d-flex align-items-center px-3">
                                     <!--begin::Avatar-->
                                     <div class="symbol symbol-50px me-5">
-                                        <img alt="Logo" src="{{asset('images/users/'.Auth::user()->avatar)}}" />
+                                        <img alt="Logo" src="{{asset('storage/'.Auth::user()->avatar)}}" />
                                     </div>
                                     <!--end::Avatar-->
                                     <!--begin::Username-->
