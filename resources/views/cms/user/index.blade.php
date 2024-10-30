ea4a71f512b78b7f3b1715f645586c17bf8d55b5
Baraaabudraz <102161963+Baraaabudraz@users.noreply.github.com>
2024-10-30 10:37:03 +0200

    Update index.blade.php

--- resources/views/cms/user/index.blade.php
+++ resources/views/cms/user/index.blade.php
@@ -2,7 +2,7 @@
 @section('title',trans('dashboard_trans.Users'))
 @section('style')
     @if(App::getLocale()=='ar')
-        <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.rtl.css')}}" rel="stylesheet" type="text/css">
+                <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.rtl.css')}}" rel="stylesheet" type="text/css">
     @else
         <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
     @endif
