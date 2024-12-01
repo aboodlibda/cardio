e3582fda6b666e4d463f451056e2be42c515728d
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-01 10:52:43 +0200

    Update CouponController.php

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coupon;
 use Illuminate\Http\Request;
 
 class CouponController extends Controller
 {
     public function index()
     {
-        return view('cms.coupon.index');
+        $coupons = Coupon::query()->get();
+        return view('cms.coupon.index', compact('coupons'));
 
     }
 
@@ -20,6 +22,38 @@ class CouponController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'code'  =>'required|string',
+            'description'  =>'nullable|string',
+            'discount_value'  =>'nullable|integer',
+            'discount_type' =>'in:percentage,fixed',
+            'times_used' =>'numeric',
+            'max_used' =>'numeric',
+            'start_date' =>'string',
+            'end_date' =>'string',
+            'status' =>'in:active,inactive',
+
+        ]);
+        $data = $request->only([
+            'code', 'description', 'discount_value', 'discount_type', 'times_used', 'max_used',
+        ]);
+        $data['status'] = $request->has('status') ? 'active' : 'inactive';
+
+        $coupon = Coupon::query()->create($data);
+        if ($coupon) {
+            return response()->json([
+                'icon' => 'success',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Coupon created successfully'),
+            ]);
+
+        }else{
+            return response()->json([
+                'icon' => 'error',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Failed to create coupon'),
+            ]);
+        }
     }
 
     public function show($id)
