407985745011aeb5a7258701dede3ae95c6fac00
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-01 12:29:20 +0200

    Update TagController.php

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TagController extends Controller
 {
@@ -20,9 +21,18 @@ class TagController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|string|min:3|max:55',
+        $validator = Validator::make($request->all(), [
+            'name.*' => 'required|string|min:3|max:55',
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+                'text' => trans('dashboard_trans.Please correct the highlighted errors and try again'),
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'icon'=>'error',
+            ], 422);
+        }
 
         $data = $request->only(['name']);
 
@@ -30,19 +40,22 @@ class TagController extends Controller
 
         if ($isSaved) {
             return response()->json([
-                'success' => true,
-                'title'=>'success',
-                'icon'=>'success',
-                'text'=>trans('dashboard_trans.Tag Created Successfully'),
+
+                'title' => trans('dashboard_trans.Success'),
+                'icon' => 'success',
+                'confirmButtonText' => trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Tag Created Successfully'),
             ]);
-        }else{
+        } else {
             return response()->json([
-                'title'=>'success',
-                'icon'=>'success',
-                'text'=>trans('dashboard_trans.Failed to Create Tag'),
+                'title' => trans('dashboard_trans.Error'),
+                'icon' => 'error',
+                'confirmButtonText' => trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Failed to Create Tag'),
             ]);
         }
     }
+
 
     public function show($id)
     {
@@ -54,9 +67,65 @@ class TagController extends Controller
 
     public function update(Request $request, $id)
     {
+        $request->request->add(['id' => $id]);
+
+        $validator = Validator::make($request->all(), [
+            'id'=>'required|integer|exists:tags,id',
+            'name.*' => 'required|string|min:3|max:55',
+        ]);
+
+        $tag = Tag::query()->findOrFail($id);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+                'text' => trans('dashboard_trans.Please correct the highlighted errors and try again'),
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'icon'=>'error',
+            ], 422);
+        }
+
+        $data = $request->only(['name']);
+
+        $isUpdated = $tag->update($data);
+
+        if ($isUpdated) {
+            return response()->json([
+                'title' => trans('dashboard_trans.Success'),
+                'icon' => 'success',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Tag Updated Successfully'),
+            ]);
+        } else {
+            return response()->json([
+                'title' => trans('dashboard_trans.Error'),
+                'icon' => 'error',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+                'text' => trans('dashboard_trans.Failed to Update Tag'),
+            ]);
+        }
     }
 
     public function destroy($id)
     {
+        $tag = Tag::query()->findOrFail($id);
+        $isDeleted = $tag->delete();
+        if ($isDeleted) {
+            return response()->json([
+                'icon' =>'success',
+                'text' => trans('dashboard_trans.Tag Deleted Successfully'),
+                'title' => 'success',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+
+            ]);
+        }else{
+            return response()->json([
+                'icon' =>'error',
+                'text' => trans('dashboard_trans.Failed to Delete Tag'),
+                'title' => 'error',
+                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
+
+            ]);
+        }
     }
 }
