0eade73d759038a65cb8ef8fee1d876dcdb5b868
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-01 10:53:35 +0200

    Update master.blade.php

--- resources/views/cms/layout/master.blade.php
+++ resources/views/cms/layout/master.blade.php
@@ -1574,7 +1574,7 @@
 {{--<!--end::Vendors Javascript-->--}}
 {{--<!--begin::Custom Javascript(used for this page only)-->--}}
 {{--<script src="{{asset('assets/js/widgets.bundle.js')}}"></script>--}}
-{{--<script src="{{asset('assets/js/custom/widgets.js')}}"></script>--}}
+<script src="{{asset('assets/js/custom/widgets.js')}}"></script>
 {{--<script src="{{asset('assets/js/custom/apps/chat/chat.js')}}"></script>--}}
 {{--<script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>--}}
 <!--end::Global Javascript Bundle-->
