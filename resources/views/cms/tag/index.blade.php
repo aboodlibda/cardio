71cd57a35fd8bd86d3b33511d1898b0b53b77be9
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-01 12:29:32 +0200

    Update index.blade.php

--- resources/views/cms/tag/index.blade.php
+++ resources/views/cms/tag/index.blade.php
@@ -140,7 +140,7 @@
                         </thead>
                         <tbody class="fw-semibold text-gray-600">
                         @foreach($tags as $tag)
-                        <tr>
+                        <tr data-tag-id="{{ $tag->id }}">
                             <td>
                                 <div class="form-check form-check-sm form-check-custom form-check-solid">
                                     <input class="form-check-input" type="checkbox" value="1" />
@@ -170,7 +170,7 @@
                                 <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
                                     <!--begin::Menu item-->
                                     <div class="menu-item px-3">
-                                        <a href="../../demo3/dist/apps/ecommerce/catalog/add-category.html" class="menu-link px-3">{{trans('dashboard_trans.Edit')}}</a>
+                                        <a href="#" class="menu-link px-3" data-bs-toggle="modal" data-bs-target="#kt_modal_edit_tag{{$tag->id}}">{{trans('dashboard_trans.Edit')}}</a>
                                     </div>
                                     <!--end::Menu item-->
                                     <!--begin::Menu item-->
@@ -182,10 +182,79 @@
                                 <!--end::Menu-->
                             </td>
                         </tr>
+                        <!--begin::Modal - Edit Tag-->
+                        <div class="modal fade" id="kt_modal_edit_tag{{ $tag->id }}" tabindex="-1" aria-hidden="true">
+                            <!--begin::Modal dialog-->
+                            <div class="modal-dialog modal-dialog-centered mw-650px">
+                                <!--begin::Modal content-->
+                                <div class="modal-content">
+                                    <!--begin::Modal header-->
+                                    <div class="modal-header">
+                                        <!--begin::Modal title-->
+                                        <h2 class="fw-bold">{{trans('dashboard_trans.Edit tag')}}</h2>
+                                        <!--end::Modal title-->
+                                        <!--begin::Close-->
+                                        <div class="btn btn-icon btn-sm btn-active-icon-primary" data-kt-tags-modal-action="close">
+                                            <i class="ki-duotone ki-cross fs-1">
+                                                <span class="path1"></span>
+                                                <span class="path2"></span>
+                                            </i>
+                                        </div>
+                                        <!--end::Close-->
+                                    </div>
+                                    <!--end::Modal header-->
+                                    <!--begin::Modal body-->
+                                    <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
+                                        <!--begin::Form-->
+                                        <form id="kt_modal_edit_tag_form" class="form" data-kt-redirect="{{route('tags.index')}}" action="{{route('tags.update',$tag->id)}}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            @foreach(config('lang') as $key=>$lang)
+                                                <div class="fv-row mb-7">
+                                                    <!--begin::Label-->
+                                                    <label class="fs-6 fw-semibold form-label mb-2">
+                                                        <span class="required">{{trans('dashboard_trans.Name')}} ({{$lang}})</span>
+                                                        <span class="ms-2" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-html="true" data-bs-content="{{trans('dashboard_trans.The tag name is required')}}">
+															<i class="ki-duotone ki-information fs-7">
+																<span class="path1"></span>
+																<span class="path2"></span>
+																<span class="path3"></span>
+															</i>
+														</span>
+                                                    </label>
+                                                    <!--end::Label-->
+                                                    <!--begin::Input-->
+                                                    <input class="form-control form-control-solid" placeholder="{{trans('dashboard_trans.Enter the name of tag')}}" name="name[{{$key}}]" value="{{$tag->getTranslation('name',$key)}}" />
+                                                    <!--end::Input-->
+                                                    <div id="name-{{ $key }}-error" class="error-message"></div>
+                                                </div>
+                                            @endforeach
+                                            <!--end::Input group-->
+                                            <!--begin::Actions-->
+                                            <div class="text-center pt-15">
+                                                <button type="reset" class="btn btn-light me-3" data-kt-tags-modal-action="cancel">{{trans('dashboard_trans.Cancel')}}</button>
+                                                <button type="submit" class="btn btn-primary" data-kt-tags-modal-action="submit">
+                                                    <span class="indicator-label">{{trans('dashboard_trans.Submit')}}</span>
+                                                    <span class="indicator-progress">{{trans('dashboard_trans.Please wait')}}...
+														<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                                                </button>
+                                            </div>
+                                            <!--end::Actions-->
+                                        </form>
+                                        <!--end::Form-->
+                                    </div>
+                                    <!--end::Modal body-->
+                                </div>
+                                <!--end::Modal content-->
+                            </div>
+                            <!--end::Modal dialog-->
+                        </div>
+                        <!--end::Modal - Edit Tag-->
                         @endforeach
                         </tbody>
                         <!--end::Table body-->
                     </table>
+
                     <!--end::Table-->
                 </div>
                 <!--end::Card body-->
@@ -200,6 +269,7 @@
     <script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
     <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/tags.js')}}"></script>
     <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/save-tag.js')}}"></script>
+    <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/edit-tag.js')}}"></script>
     <script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
 @endsection
 
