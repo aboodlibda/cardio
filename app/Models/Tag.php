67b064f20c98507b07ef0aec2fe1e619d68d6939
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-01 10:52:56 +0200

    Update Tag.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Translatable\HasTranslations;
 
 class Tag extends Model
 {
+    use HasTranslations;
+    protected $translatable = ['name'];
     protected $guarded = [];
 
     public function products(): BelongsToMany
     {
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class, 'product_tags', 'tag_id', 'product_id');
     }
 }
