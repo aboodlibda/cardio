e25472a4e008694edbd7df071f5713d71cdfc4e6
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-21 11:31:55 +0200

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ControllerHelper;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Tag;
@@ -19,7 +20,7 @@ class ProductController extends Controller
     }
     public function index()
     {
-        $products = Product::query()->with(['categories','tags','variants','images','coupons','orderItems'])->get();
+        $products = Product::query()->with(['categories','tags','variants','images','coupons','orderItems'])->latest()->get();
         return view('cms.product.index',compact('products'));
 
     }
@@ -31,55 +32,70 @@ class ProductController extends Controller
         return view('cms.product.create',compact('categories','tags'));
     }
 
-    public function store(Request $request)
+
+
+    // Adjusted store method to handle image uploading
+    public function store(Request $request,ImageUploadService $imageUploadService)
     {
-        $validator = Validator::make($request->all(),[
-            'title.*'       => 'required|string|min:3|max:100',
-            'description.*' => 'nullable|string',
-            'price'       => 'required|integer|numeric',
-            'status'      => 'in:published,unpublished,draft',
-            'user_id'     => 'required|int|exists:users,id',
-            'slug'        => 'required|string',
-            'quantity'    => 'required|numeric',
-            'SKU'         => 'required|string|min:5|max:30',
-            'category_id' => 'required|int|exists:categories,id',
-            'discount_type'=>'required|in:no_discount,percentage,fixed_price',
-            'tax_type'     =>'required|in:free,taxable_goods,downloadable_product',
-            'vat_amount'   =>'required|numeric',
-            'images'       => 'nullable|image|mimes:jpeg,png,jpg,gif,svg',
+        $validator = Validator::make($request->all(), [
+            'name.*'          => 'required|string|min:3|max:100',
+            'description.*'   => 'nullable|string',
+            'price'           => 'required|integer|numeric',
+            'status'          => 'required|in:published,unpublished,draft',
+            'slug'            => 'required|string|unique:products,slug',
+            'quantity'        => 'required|numeric',
+            'SKU'             => 'required|string|min:5|max:30',
+            'category_id'     => 'required|int|exists:categories,id',
+            'discount_type'   => 'required|in:no_discount,percentage,fixed_price',
+            'tax_type'        => 'required|in:free,taxable_goods,downloadable_product',
+            'vat_amount'      => 'nullable|numeric',
+            'discounted_price'=> 'nullable|numeric',
+            'images.*'        => 'nullable|image|mimes:jpeg,png,jpg,gif,svg',
+            'tag_id'          => 'nullable|string|exists:tags,id',
         ]);
+
         if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+            return ControllerHelper::generateResponse($validator->errors(), 'error', trans('dashboard_trans.Please correct the highlighted errors and try again'), 422);
         }
+
         $data = $request->only([
-            'title' , 'description' , 'price' , 'status' , 'user_id' , 'slug' , 'quantity' , 'SKU'
+            'name', 'description', 'price', 'status', 'user_id', 'slug', 'quantity', 'SKU', 'tax_type', 'vat_amount', 'discount_type', 'discounted_price'
         ]);
 
-        $data['user_id'] = auth()->user()->id;
+        $data['user_id'] = auth()->check() ? auth()->user()->id : null;
 
-        $data['status'] = $request->input('status') === 'draft' ? 'draft' : ($request->has('status') ? 'published' : 'unpublished');
+        $request->status = $request->input('status') === 'draft' ? 'draft' : ($request->has('status') ? 'published' : 'unpublished');
 
-        $image = $this->imageUploadService->upload($request, 'image', 'images/products');
-        $data['image'] = $image;
 
         $product = Product::query()->create($data);
 
-        $product->categories()->attach($request->category_id);
+        if ($request->has('tag_id')) {
+            $product->tags()->attach($request->tag_id);
+        }
 
-        if ($product){
-            return response()->json([
-                'icon' => 'success',
-                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
-                'text' => trans('dashboard_trans.Product created successfully'),
-            ]);
-        }else{
-            return response()->json([
-                'icon' => 'error',
-                'confirmButtonText'=>trans('dashboard_trans.Ok, got it!'),
-                'text' => trans('dashboard_trans.Failed to create product'),
+        if ($request->has('category_id')) {
+            $product->categories()->attach($request->category_id);
+        }
+
+        // Move images from temporary storage to permanent directory
+        $uploadedFiles = session()->get('uploaded_files', []);
+        $savedImages = $imageUploadService->moveImages($uploadedFiles,'images/products');
+
+        foreach ($savedImages as $imagePath) {
+            $product->images()->create([
+                'product_id' => $product->id,
+                'image' => $imagePath,
             ]);
         }
 
+        // Clear the session
+        session()->forget('uploaded_files');
+
+        if ($product) {
+            return ControllerHelper::generateResponse('', 'success', trans('dashboard_trans.Product created successfully'), 201);
+        } else {
+            return ControllerHelper::generateResponse('', 'error', trans('dashboard_trans.Failed to create product'), 500);
+        }
     }
 
     public function show($id)
@@ -88,30 +104,127 @@ class ProductController extends Controller
 
     public function edit($id)
     {
+        $product = Product::query()->findOrFail($id);
+        $categories = Category::query()->get();
+        $tags = Tag::query()->latest()->get(['id','name']);
+        return view('cms.product.edit',compact('product','categories','tags'));
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, ImageUploadService $imageUploadService)
     {
+        $request->request->add(['id' => $request->$id]);
+
+        $validator = Validator::make($request->all(),[
+            'id'            =>'required|int|exists:products,id',
+            'name.*'        => 'required|string|min:3|max:100',
+            'description.*' => 'nullable|string',
+            'price'         => 'required|integer|numeric',
+            'status'        => 'in:published,unpublished,draft',
+//            'user_id'     => 'required|int|exists:users,id',
+            'slug'          => 'required|string|unique:products,slug',
+            'quantity'      => 'required|numeric',
+            'SKU'           => 'required|string|min:5|max:30',
+            'category_id'   => 'required|int|exists:categories,id',
+            'discount_type'    => 'required|in:no_discount,percentage,fixed_price',
+            'tax_type'         => 'required|in:free,taxable_goods,downloadable_product',
+            'vat_amount'       => 'nullable|numeric',
+            'discounted_price' => 'nullable|numeric',
+            'images.*'         => 'nullable|image|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+        if ($validator->fails()) {
+            return ControllerHelper::generateResponse($validator->errors(), 'error', trans('dashboard_trans.Please correct the highlighted errors and try again'),422);
+        }
+        $product = Product::query()->findOrFail($id);
+
+        $data = $request->only([
+            'name' , 'description' , 'price' , 'status' , 'user_id' , 'slug' , 'quantity' , 'SKU','tax_type','vat_amount','discount_type','discounted_price'
+        ]);
+
+        $data['user_id'] = auth()->check() ? auth()->user()->id : null;
+
+
+        $data['status'] = $request->input('status') === 'draft' ? 'draft' : ($request->has('status') ? 'published' : 'unpublished');
+
+
+        $isUpdated = $product->update($data);
+
+        // Move images from temporary storage to permanent directory
+        $uploadedFiles = session()->get('uploaded_files', []);
+        $savedImages = $imageUploadService->moveImages($uploadedFiles,'images/products');
+
+        foreach ($savedImages as $imagePath) {
+            $product->images()->create([
+                'product_id' => $product->id,
+                'image' => $imagePath,
+            ]);
+        }
+
+
+        if ($request->has('tag_id')) {
+            $product->tags()->sync($request->tag_id);
+        }
+
+        if ($request->has('category_id')) {
+            $product->categories()->sync($request->category_id);
+        }
+
+        // Clear the session
+        session()->forget('uploaded_files');
+
+        if ($isUpdated){
+            return ControllerHelper::generateResponse('','success',trans('dashboard_trans.Product updated successfully'),200);
+        }else{
+            return ControllerHelper::generateResponse('','error',trans('dashboard_trans.Failed to update product'),500);
+
+        }
     }
 
     public function destroy($id)
     {
+        $product = Product::query()->find($id);
+
+        if ($product) {
+            foreach ($product->images as $image) {
+                if ($image) {
+                    $imagePath = public_path('storage/' . $image->image);
+                    if (file_exists($imagePath)) {
+                        unlink($imagePath);
+                    }
+                }
+            }
+
+            $product->delete();
+
+            return ControllerHelper::generateResponse('','success',trans('dashboard_trans.Product deleted successfully'),201);
+        }
+        return ControllerHelper::generateResponse('','error',trans('dashboard_trans.Failed to delete this product'),500);
     }
 
-    public function uploadImage(Request $request)
+    public function storeMedia(Request $request, ImageUploadService $imageUploadService)
     {
-        // Validate if the file is an image
-        $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:10240' // max size 10MB
-        ]);
+        $path = storage_path('tmp/uploads');
 
-        // Upload the image using the imageUploadService
-        $imagePath = $this->imageUploadService->uploadImages($request, 'image', 'images/products');
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
 
-        // Return a response with the path to the uploaded image
+        $file = $request->file('images');
+
+        if (!$file) {
+            return response()->json(['error' => 'No file uploaded.'], 400);
+        }
+
+        $name = uniqid() . '_' . trim($file->hashName());
+        $file->move($path, $name);
+
+        // Store the file path in the session (or database)
+        $uploadedFiles = session()->get('uploaded_files', []);
+        $uploadedFiles[] = $name; // Store the filename
+        session()->put('uploaded_files', $uploadedFiles);
+
         return response()->json([
-            'path' => $imagePath,
-            'message' => 'Image uploaded successfully'
+            'name' => $name,
+            'original_name' => $file->hashName(),
         ]);
     }
 }
