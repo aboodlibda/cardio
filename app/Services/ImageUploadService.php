dbb5ea96ce56ca76152c846e4567dc42c6238c40
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-21 11:32:19 +0200

    Update ImageUploadService.php

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -3,7 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Storage;
 
 
 class ImageUploadService
@@ -21,18 +21,69 @@ class ImageUploadService
         }
 
     }
-    public function uploadImages(Request $request, $fileKey  , $directory)
+//    public function uploadImages(Request $request, $fileKey  , $directory)
+//    {
+//        if ($request->hasFile($fileKey)) {
+//            $images = $request->file($fileKey);
+//            $imageName = time() . '_' . uniqid() . '.' . $images->getClientOriginalExtension();
+//            $path = $images->storeAs($directory, $imageName, 'public');
+//            return $path;
+//        }else{
+//            return null;
+//
+//        }
+//
+//    }
+    public function uploadImages($request, $inputName, $folder)
     {
-        if ($request->hasFile($fileKey)) {
-            $images = $request->file($fileKey);
-            $imageName = time() . '_' . uniqid() . '.' . $images->getClientOriginalExtension();
-            $path = $images->storeAs($directory, $imageName, 'public');
-            return $path;
-        }else{
-            return null;
+        $uploadedImages = [];
 
+        if ($request->hasFile($inputName)) {
+            foreach ($request->file($inputName) as $file) {
+                $fileName = time().'_'.uniqid() . '_' . $file->getClientOriginalExtension();
+                $filePath = $file->storeAs($folder, $fileName, 'public');
+                $uploadedImages[] = $filePath;
+            }
         }
 
+        return $uploadedImages;
+    }
+
+//    public function moveImages(array $uploadedFiles, $destinationFolder)
+//    {
+//        $savedFiles = [];
+//
+//        foreach ($uploadedFiles as $fileName) {
+//            $sourcePath = storage_path('tmp/uploads/' . $fileName);
+//            $destinationPath = storage_path($destinationFolder . '/' . $fileName);
+//
+//            if (file_exists($sourcePath)) {
+//                if (!file_exists(storage_path($destinationFolder))) {
+//                    mkdir(storage_path($destinationFolder), 0777, true);
+//                }
+//
+//                rename($sourcePath, $destinationPath);
+//
+//                $savedFiles[] = $destinationFolder . '/' . $fileName;
+//            }
+//        }
+//
+//        return $savedFiles;
+//    }
+
+    public function moveImages(array $uploadedFiles, $destinationFolder)
+    {
+        $savedFiles = [];
+
+        foreach ($uploadedFiles as $fileName) {
+            $sourcePath = storage_path('tmp/uploads/' . $fileName);
+            if (file_exists($sourcePath)) {
+                Storage::disk('public')->putFileAs($destinationFolder, $sourcePath, $fileName);
+                $savedFiles[] = $destinationFolder . '/' . $fileName;
+            }
+        }
+
+        return $savedFiles;
     }
 
 }
