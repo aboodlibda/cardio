83db7c76d5677b0fa58e73b425af8483d45b5a65
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-21 11:32:23 +0200

    Update 2024_10_06_101728_create_coupons_table.php

--- database/migrations/2024_10_06_101728_create_coupons_table.php
+++ database/migrations/2024_10_06_101728_create_coupons_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
-            $table->text('description')->nullable();
+            $table->json('description')->nullable();
             $table->string('discount_value');
             $table->enum('discount_type',['percentage','fixed']);
             $table->string('times_used')->default(0);
