940f9bb019d7e3b650fb7818256a287e3c48e93e
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-12-24 11:22:01 +0200

    Update master.blade.php

--- resources/views/cms/layout/master.blade.php
+++ resources/views/cms/layout/master.blade.php
@@ -151,7 +151,16 @@
 
                                 <!--end:Menu item-->
                                 <!--begin:Menu item-->
-
+                                <div class="menu-item">
+                                    <!--begin:Menu link-->
+                                    <a class="menu-link" href="{{ route('attributes.index') }}"  data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right">
+												<span class="menu-bullet">
+													<span class="bullet bullet-dot"></span>
+												</span>
+                                        <span class="menu-title">{{trans('dashboard_trans.Attributes')}}</span>
+                                    </a>
+                                    <!--end:Menu link-->
+                                </div>
                                 <!--end:Menu item-->
                             </div>
                             <!--end:Menu sub-->
