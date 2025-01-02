f8df209fbf9eb03e56cc680faf815d53dfb79560
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-01-02 12:23:01 +0200

    Update index.blade.php

--- resources/views/cms/attributes/index.blade.php
+++ resources/views/cms/attributes/index.blade.php
@@ -3,7 +3,7 @@
 @section('style')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @if(App::getLocale()=='ar')
-        <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.rtl.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.rtl.css')}}" rel="stylesheet" type="text/css" />
     @else
         <link href="{{asset('assets/plugins/custom/datatables/datatables.bundle.css')}}" rel="stylesheet" type="text/css" />
     @endif
@@ -62,60 +62,17 @@
                     <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_ecommerce_attribute_table" >
                         <thead>
                         <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                            <th class="w-10px pe-2">
+                            <th>
                                 <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                                    <input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_ecommerce_attribute_table .form-check-input" value="1" />
+                                    <input class="form-check-input" type="checkbox" data-kt-check="true" id="select-all"  data-kt-check-target="#kt_ecommerce_attribute_table .form-check-input" value="1" />
                                 </div>
                             </th>
-                            <th class="min-w-250px">{{trans('dashboard_trans.Attributes')}}</th>
-                            <th class="min-w-150px">{{trans('dashboard_trans.Status')}}</th>
+                            <th class="min-w-250px">{{trans('dashboard_trans.Name')}}</th>
+                            <th class="min-w-150px">{{trans('dashboard_trans.Created Date')}}</th>
                             <th class="text-end min-w-70px">{{trans('dashboard_trans.Actions')}}</th>
                         </tr>
                         </thead>
                         <tbody class="fw-semibold text-gray-600">
-                        @foreach($attributes as $attribute)
-                            <tr data-attribute-id="{{ $attribute->id }}">
-                                <td>
-                                    <div class="form-check form-check-sm form-check-custom form-check-solid">
-                                        <input class="form-check-input" type="checkbox" value="1" />
-                                    </div>
-                                </td>
-                                <td class="min-w-20px">
-                                    <div class="d-flex">
-                                        <div class="ms-5">
-                                            <!--begin::Title-->
-                                            <a href="{{ route('attributes.edit',$attribute->id) }}" class="text-gray-800 text-hover-primary fs-5 fw-bold mb-1" data-kt-ecommerce-attribute-filter="attribute_name">{{ $attribute->name }}</a>
-                                            <!--end::Title-->
-                                            <!--begin::Description-->
-{{--                                            <div class="text-muted fs-7 fw-bold">{{ $attribute->description }}</div>--}}
-                                            <!--end::Description-->
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-
-                                </td>
-                                <td class="text-end">
-                                    <a href="#" class="btn btn-sm btn-light btn-active-light-primary btn-flex btn-center" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">{{trans('dashboard_trans.Actions')}}
-                                        <i class="ki-duotone ki-down fs-5 ms-1"></i></a>
-                                    <!--begin::Menu-->
-                                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3">
-                                            <a href="{{ route('attributes.edit',$attribute->id) }}" class="menu-link px-3">{{trans('dashboard_trans.Edit')}}</a>
-                                        </div>
-                                        <!--end::Menu item-->
-                                        <!--begin::Menu item-->
-                                        <div class="menu-item px-3" >
-                                            <a href="#" class="menu-link px-3" data-kt-ecommerce-attribute-filter="delete_row">{{trans('dashboard_trans.Delete')}}</a>
-
-                                        </div>
-                                        <!--end::Menu item-->
-                                    </div>
-                                    <!--end::Menu-->
-                                </td>
-                            </tr>
-                        @endforeach
                         </tbody>
                         <!--end::Table body-->
                     </table>
@@ -133,5 +90,4 @@
     <script src="{{asset('assets/plugins/custom/datatables/datatables.bundle.js')}}"></script>
     <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/attributes.js')}}"></script>
     <script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
-
 @endsection
