13150913ea08157ae073cbff80dcda20d501516f
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-01-02 12:22:37 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attribute;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,15 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+         Attribute::factory(50000)->create();
+//         User::factory(10)->create();
 //
 //        User::factory()->create([
 //            'name' => 'Test User',
 //            'email' => 'test@example.com',
 //        ]);
-
-          $this->call(RoleSeeder::class);
-          $this->call(UserSeeder::class);
-          $this->call(PermissionSeeder::class);
+//
+//          $this->call(RoleSeeder::class);
+//          $this->call(UserSeeder::class);
+//          $this->call(PermissionSeeder::class);
     }
 }
