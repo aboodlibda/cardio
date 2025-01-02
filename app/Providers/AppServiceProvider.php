386885fe7f6c6375a1726995ab0b7a5ba1716d7c
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-01-02 12:22:10 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Yajra\DataTables\DataTables;
 
 class AppServiceProvider extends ServiceProvider
 {
